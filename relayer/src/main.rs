d52eee52349e4701a06c81d6dc738d737305d690
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-07-04 19:47:12 +0900

    updated main.rs for dummy data

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -19,24 +19,34 @@ enum Commands {
     Relay(relay::RelayOpts),
 }
 
-fn main() {
-    env_logger::init();
-    let args = Cli::parse();
+#[tokio::main]
+async fn main() {
+    let opts = relay::RelayOpts::parse();
 
-    info!("Starting hvm relayer");
-
-    match args.command {
-        Commands::Connect(opts) => {
-            let rt = runtime::get_rt(opts.threads);
-            rt.block_on(connect::start(opts));
-        }
-        Commands::FetchCalldata(opts) => {
-            let rt = runtime::get_rt(opts.threads);
-            rt.block_on(calldata::fetch(opts));
-        }
-        Commands::Relay(opts) => {
-            let rt = runtime::get_rt(opts.threads);
-            rt.block_on(relay::start(opts));
-        }
+    if let Err(e) = relay::run(opts).await {
+        eprintln!("Relayer failed to start: {:?}", e);
+        std::process::exit(1);
     }
 }
+
+// fn main() {
+//     env_logger::init();
+//     let args = Cli::parse();
+
+//     info!("Starting hvm relayer");
+
+//     match args.command {
+//         Commands::Connect(opts) => {
+//             let rt = runtime::get_rt(opts.threads);
+//             rt.block_on(connect::start(opts));
+//         }
+//         Commands::FetchCalldata(opts) => {
+//             let rt = runtime::get_rt(opts.threads);
+//             rt.block_on(calldata::fetch(opts));
+//         }
+//         Commands::Relay(opts) => {
+//             let rt = runtime::get_rt(opts.threads);
+//             rt.block_on(relay::start(opts));
+//         }
+//     }
+// }
