7e98c969d0e844c5e4d6e17996eef509cc12e514
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-07-17 15:47:29 +0900

    updated prover zk snark

--- offchain-labs/src/prover/libs/zk_snark.rs
+++ offchain-labs/src/prover/libs/zk_snark.rs
@@ -1,30 +1,64 @@
 use crate::error::HVMError;
 use crate::zk_rollup::Proof;
+use crate::sequencer::Batch;
 use super::super::ProverLibs;
-use bellman::{groth16, Circuit, ConstraintSystem, SynthesisError};
+use bellman::{
+    Circuit, ConstraintSystem, SynthesisError,
+    groth16::{
+        create_random_proof, generate_random_parameters,
+        Parameters, Proof as BMProof,
+    },
+};
 use bls12_381::Bls12;
 use rand::thread_rng;
+use serde::{Serialize, Deserialize};
 
 pub struct ZKSnarkLibs {
-    proving_key: groth16::ProvingKey<Bls12>,
+    proving_key: Parameters<Bls12>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SerializableProof(#[serde(with = "proof_serde")] BMProof<Bls12>);
+
+mod proof_serde {
+    use super::*;
+    use serde::{Serializer, Deserializer};
+
+    pub fn serialize<S>(proof: &BMProof<Bls12>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut bytes = Vec::new();
+        proof.write(&mut bytes).map_err(serde::ser::Error::custom)?;
+        serializer.serialize_bytes(&bytes)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<BMProof<Bls12>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let bytes = Vec::<u8>::deserialize(deserializer)?;
+        BMProof::read(&bytes[..]).map_err(serde::de::Error::custom)
+    }
 }
 
 impl ZKSnarkLibs {
     pub fn new() -> Self {
         let circuit = DummyCircuit { x: None };
-        let params = groth16::generate_random_parameters::<Bls12, _, _>(circuit, &mut thread_rng()).unwrap();
+        let params = generate_random_parameters::<Bls12, _, _>(circuit, &mut thread_rng()).unwrap();
         Self { proving_key: params }
     }
 }
 
 impl ProverLibs for ZKSnarkLibs {
-    fn generate_proof(&self, input: &[u8]) -> Result<Proof, HVMError> {
-        let circuit = DummyCircuit { x: Some(input[0] as u64) };
+    fn generate_proof(&self, batch: &Batch) -> Result<Proof, HVMError> {
+        let circuit = DummyCircuit { x: Some(bls12_381::Scalar::from(batch.transactions().len() as u64)) };
         
-        let proof = groth16::create_random_proof(circuit, &self.proving_key, &mut thread_rng())
+        let proof = create_random_proof(circuit, &self.proving_key, &mut thread_rng())
             .map_err(|e| HVMError::Prover(format!("Failed to generate proof: {}", e)))?;
         
-        let proof_bytes = bincode::serialize(&proof)
+        let serializable_proof = SerializableProof(proof);
+        let proof_bytes = bincode::serialize(&serializable_proof)
             .map_err(|e| HVMError::Prover(format!("Failed to serialize proof: {}", e)))?;
         
         Ok(Proof::new(proof_bytes))
@@ -32,7 +66,7 @@ impl ProverLibs for ZKSnarkLibs {
 }
 
 struct DummyCircuit {
-    x: Option<u64>,
+    x: Option<bls12_381::Scalar>,
 }
 
 impl Circuit<bls12_381::Scalar> for DummyCircuit {
