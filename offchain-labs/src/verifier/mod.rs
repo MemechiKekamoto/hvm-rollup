d7dc8bcace233d787d727da4a4ffe69dd3bcecda
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-07-19 16:48:12 +0900

    optimzed verifier structure

--- offchain-labs/src/verifier/mod.rs
+++ offchain-labs/src/verifier/mod.rs
@@ -1,27 +1,15 @@
-mod libs;
-
 use crate::error::HVMError;
 use crate::zk_rollup::Proof;
-use libs::ZKSnarkLibs;
 
-pub struct Verifier {
-    strategy: Box<dyn VerifierLibs>,
-}
+pub struct Verifier;
 
 impl Verifier {
-    pub fn new(strategy: Box<dyn VerifierLibs>) -> Self {
-        Self { strategy }
-    }
-
     pub fn verify_proof(&self, proof: &Proof) -> Result<bool, HVMError> {
-        self.strategy.verify_proof(proof)
+        println!("Verifying proof: {:?}", proof);
+        Ok(true)
     }
-}
-
-pub trait VerifierLibs {
-    fn verify_proof(&self, proof: &Proof) -> Result<bool, HVMError>;
 }
 
 pub fn create_zk_snark_verifier() -> Verifier {
-    Verifier::new(Box::new(ZKSnarkLibs::new()))
+    Verifier
 }
