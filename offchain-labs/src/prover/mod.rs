4cea5ac2e5736aa0c8cb0e9ef8fc9b6d699aa6f2
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-07-19 16:47:54 +0900

    optimzed prover structure

--- offchain-labs/src/prover/mod.rs
+++ offchain-labs/src/prover/mod.rs
@@ -1,28 +1,15 @@
-mod libs;
-
 use crate::error::HVMError;
 use crate::zk_rollup::Proof;
 use crate::sequencer::Batch;
-use libs::ZKSnarkLibs;
 
-pub struct Prover {
-    strategy: Box<dyn ProverLibs>,
-}
+pub struct Prover;
 
 impl Prover {
-    pub fn new(strategy: Box<dyn ProverLibs>) -> Self {
-        Self { strategy }
+    pub fn generate_proof(&self, _batch: &Batch) -> Result<Proof, HVMError> {
+        Ok(Proof::new(vec![1, 2, 3, 4]))
     }
-
-    pub fn generate_proof(&self, batch: &Batch) -> Result<Proof, HVMError> {
-        self.strategy.generate_proof(batch)
-    }
-}
-
-pub trait ProverLibs {
-    fn generate_proof(&self, batch: &Batch) -> Result<Proof, HVMError>;
 }
 
 pub fn create_zk_snark_prover() -> Prover {
-    Prover::new(Box::new(ZKSnarkLibs::new()))
+    Prover
 }
