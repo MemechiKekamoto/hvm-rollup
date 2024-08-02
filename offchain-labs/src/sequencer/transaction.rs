dc63c99b7800e8dd896f9738c2afcb861e9a832e
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-08-02 17:53:12 +0900

    added program id in tx struct

--- offchain-labs/src/sequencer/transaction.rs
+++ offchain-labs/src/sequencer/transaction.rs
@@ -4,12 +4,13 @@ use serde::{Serialize, Deserialize};
 pub struct Transaction {
     pub sender: String,
     pub recipient: String,
-    pub amount: u64,
+    pub amount: Vec<u8>,
     pub nonce: u64,
+    pub program_id: String,
 }
 
 impl Transaction {
-    pub fn new(sender: String, recipient: String, amount: u64, nonce: u64) -> Self {
-        Self { sender, recipient, amount, nonce }
+    pub fn new(sender: String, recipient: String, amount: Vec<u8>, nonce: u64, program_id: String) -> Self {
+        Self { sender, recipient, amount, nonce, program_id }
     }
 }
