841e43729048bf35c6608d9b86dfd62d72a48760
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-08-09 17:54:00 +0900

    added the cmds for offchain labs

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
-use hvm_relayer::{calldata, connect, relay, runtime};
+use hvm_relayer::{commands, calldata, connect, relay, runtime, offchain_lab};
 use log::info;
 
 #[derive(Debug, Parser)]
-#[command(name = "hvm_relayer", about = "offchain relayer", version)]
+#[command(name = "hvm_relayer", about = "HVM offchain relayer", version)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -17,14 +17,25 @@ enum Commands {
     FetchCalldata(calldata::FetchOpts),
     #[command(arg_required_else_help = true)]
     Relay(relay::RelayOpts),
+    #[command(arg_required_else_help = true)]
+    OffchainLab(offchain_lab::OffchainLabOpts),
 }
 
 #[tokio::main]
-async fn main() {
-    let opts = relay::RelayOpts::parse();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
 
-    if let Err(e) = relay::run(opts).await {
-        eprintln!("Relayer failed to start: {:?}", e);
-        std::process::exit(1);
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Connect(opts) => connect::start(opts).await?,
+        Commands::FetchCalldata(opts) => {
+            let calldata = calldata::fetch(opts).await?;
+            info!("Fetched {} calldata items", calldata.len());
+        }
+        Commands::Relay(opts) => relay::run(opts).await?,
+        Commands::OffchainLab(opts) => offchain_lab::run(opts).await?,
     }
+
+    Ok(())
 }
